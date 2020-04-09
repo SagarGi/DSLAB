8f070ce22761aca8d06f374fad946c12bd1aac49
SagarGi <sagargurung1001@gmail.com>
2020-04-09 10:59:10 +0545

    SingleNumber

---
+++ leetcode/SingleNumber.java
@@ -0,0 +1,46 @@
+// Input: [4,1,2,1,2]
+// Output: 4
+
+public class SingleNumber
+{
+    public int singleNumber(int[] numbers)
+    { 
+        int count[] = new int[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++)
+        {
+            count[i] = 0;
+        }
+
+        for(int i = 0; i < numbers.length; i++)
+        {
+            for(int j = 0; j < numbers.length; j++)
+            {
+                if(numbers[i] == numbers[j])
+                {
+                    count[i]++;
+                }
+            }
+        }
+
+        int index = 0;
+
+        for(int i = 0; i < numbers.length; i++)
+        {
+            if(count[i] == 1)
+            {
+                index = i;
+            }
+        }
+        return numbers[index];
+    }
+    public static void main(String [] args)
+    {
+        int []numbers = {4,5,4,1,2,1,2};
+        SingleNumber sn = new SingleNumber();
+        int answerNumber = sn.singleNumber(numbers);
+        System.out.println("The number is :" + answerNumber);
+    }
+}
+
+// done without non- linear runtime complexity
