e5dab29ea45ac88fec1ee404ea4d308091359590
SagarGi <sagargurung1001@gmail.com>
2020-04-26 17:29:32 +0545

    anagrams

---
+++ leetcode/Anagrams.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// to check the string is anagram or not simple
+
+public class Anagrams {
+
+    public String sortString(String value)
+    {
+        char tempArray[] = value.toCharArray();
+        Arrays.sort(tempArray);
+        return new String(tempArray);
+    }
+
+    public boolean anagramOutput(String s1, String s2)
+    {
+        boolean result = false;
+
+        if(sortString(s1).equals(sortString(s2)))
+        {
+            result = true;
+        }
+
+        return result;
+
+    }
+}
+
+class AnagramsDemo
+{
+    public static void main(String args[])
+    {
+        String s1 = "batw";
+        String s2 = "tab";
+        Anagrams a = new Anagrams();
+        boolean result = a.anagramOutput(s1, s2);
+        System.out.println("Anagram = " + result);
+
+    }
+}
