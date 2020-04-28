d16094100f514031b0a5a2da864062dd0b888dd1
SagarGi <sagargurung1001@gmail.com>
2020-04-28 12:15:12 +0545

    CountAndSay

---
+++ leetcode/CountAndSay.java
@@ -0,0 +1,64 @@
+public class CountAndSay {
+
+    public String countAndSay(int n)
+    {
+        String result = "";
+
+        // we have to initialize the first 2 terms inorder to generate the other
+        String first = "1";
+        int k = 0;
+      
+
+        if(n == 1)
+        {
+            result = first;
+        }
+        else
+        {
+            while(k < n - 1)
+            {
+                // System.out.println(first);
+                int l = 0;
+                String fakeResult = "";
+                while(l < first.length())
+                {
+                    int count = 0;
+                    for(int i = l; i < first.length(); i++)
+                    {
+                        if(first.charAt(l) == first.charAt(i))
+                        {
+                            count = count + 1;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    fakeResult = fakeResult + Integer.toString(count) + first.charAt(l); 
+                    l = l + count;
+                }
+                first = fakeResult;
+                k = k + 1;
+                result = first;
+            }
+            // System.out.println(result);
+        }
+
+        return result;
+    }
+
+}
+
+class CountAndSayDemo
+{
+    public static void main(String agrs[])
+    {
+        int n = 10;
+        CountAndSay cs = new CountAndSay();
+        System.out.println(cs.countAndSay(n));
+        // cs.countAndSay(n);
+    }
+}
+
+// works perfectly fine
