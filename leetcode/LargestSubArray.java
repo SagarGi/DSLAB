7ab26afa0b237110d3b1e93d39ec86a8aab87e7f
SagarGi <sagargurung1001@gmail.com>
2020-04-30 10:58:33 +0545

    usingDynamicProgramming

--- leetcode/LargestSubArray.java
+++ leetcode/LargestSubArray.java
@@ -2,25 +2,40 @@ public class LargestSubArray {
 
     public int maxSubArray(int[] nums) {
 
-        int largest = 0;
+        int largest = Integer.MIN_VALUE;
         // using brute force
+        // for(int i = 0; i < nums.length; i++)
+        // {
+        //     for(int j = i; j  < nums.length; j++)
+        //     {
+        //         int sum = 0;
+        //         for(int k = 0; k <= j; k++)
+        //         {
+        //             sum = sum + nums[k];
+        //         }
+        //         if(sum > largest)
+        //         {
+        //             largest = sum;
+        //         }
+        //     }
+        // }
+
+        // using dynamic programming
+
         for(int i = 0; i < nums.length; i++)
         {
-            for(int j = i; j  < nums.length; j++)
+            int sum = 0;
+            for(int j = i; j < nums.length; j++)
             {
-                int sum = 0;
-                for(int k = 0; k <= j; k++)
-                {
-                    sum = sum + nums[k];
-                }
+                sum = sum + nums[j];
                 if(sum > largest)
                 {
                     largest = sum;
                 }
             }
         }
-        return largest;
-        
+
+        return largest;   
     }
 
 }
@@ -29,7 +44,7 @@ class LargestSubArrayDemo
 {
     public static void main(String args[])
     {
-        int nums[] = {1,2,3,4,-1,-1};
+        int nums[] = {-2,1,-3,4,-1,2,1,-5,4};
         LargestSubArray lg = new LargestSubArray();
         System.out.println(lg.maxSubArray(nums));
     }
