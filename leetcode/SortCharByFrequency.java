557076422f2bd26ad3f61b65b984523ddb9c278e
SagarGi <sagargurung1001@gmail.com>
2020-05-03 11:55:31 +0545

    sortcahr

--- leetcode/SortCharByFrequency.java
+++ leetcode/SortCharByFrequency.java
@@ -1,43 +1,67 @@
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.Collection;
 import java.util.Comparator;
 
 public class SortCharByFrequency {
 
     public String frequencySort(String s) {
 
-    HashMap<Integer, Character> map = new HashMap<Integer, Character>();
+    HashMap<Character, Integer> map = new HashMap<Character, Integer>();
 
-      for(int i = 0; i < 2; i++)
+      for(int i = 0; i < s.length(); i++)
       {
           int count = 0;
-         if(!map.containsValue(s.charAt(i)))
+         if(!map.containsKey(s.charAt(i)))
          {
-            for(int j = 0; j < s.length(); j++)
+            for(int j = i; j < s.length(); j++)
             {
                 if(s.charAt(i) == s.charAt(j))
                 {
                     count++;
                 }
             }
-            map.put(count, s.charAt(i));
+           
+            map.put(s.charAt(i), count);
+           
          }
          else
          {
              continue;
          }  
       }
-
-      System.out.println(map);
-
-      LinkedHashMap<Integer, Character> sorted = new LinkedHashMap<>();
+      LinkedHashMap<Character, Integer> sorted = new LinkedHashMap<>();
       map.entrySet()
       .stream()
       .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
       .forEachOrdered(x -> sorted.put(x.getKey(), x.getValue()));
-      
-    return "";
+
+    //   System.out.println(sorted);
+      Set<Character> letter = sorted.keySet(); 
+      Collection<Integer> freq = sorted.values();
+      Iterator<Integer> it = freq.iterator();
+      String result = "";
+      int frequency[] = new int[freq.size()]; 
+      int k = 0;
+      while (it.hasNext()) 
+     {
+        frequency[k] = it.next();
+        k = k + 1;
+     } 
+     int j = 0;
+     
+      for(Character c : letter)
+      {
+          for(int i = 0; i < frequency[j]; i++)
+          {
+            result = result + c;
+          }
+          j++;
+      }
+    return result;
 
     }
     
@@ -47,7 +71,7 @@ class SortCharByFrequencyDemo
 {
     public static void main (String args[])
     {
-        String s = "aberrr";
+        String s = "";
         SortCharByFrequency sf = new SortCharByFrequency();
         System.out.println(sf.frequencySort(s));
     }
