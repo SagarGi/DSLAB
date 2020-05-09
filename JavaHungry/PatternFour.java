b9d6e284665f70d28729e3930a0359670b9222a2
SagarGi <sagargurung1001@gmail.com>
2020-05-09 09:51:57 +0545

    pyramid

---
+++ JavaHungry/PatternFour.java
@@ -0,0 +1,18 @@
+public class PatternFour {
+    public static void main(String[] args) {
+        //the space method
+
+        for(int i = 1; i < 5; i++)
+        {
+            for(int j = i; j < 5; j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k = 1; k < 2 * i; k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
