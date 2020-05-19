41e8087eedac1f329f71c3c035371a60e3563270
SagarGi <sagargurung1001@gmail.com>
2020-05-19 14:07:50 +0545

    POT

---
+++ Tree/BinaryTreeTraversal.java
@@ -0,0 +1,87 @@
+
+// define a node
+class TreeNode
+{
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int data) 
+    { 
+        this.data = data; 
+        left = right = null; 
+    } 
+}
+
+public class BinaryTreeTraversal {
+
+    // refrence of tree i.e the root node
+
+    TreeNode node;
+
+    BinaryTreeTraversal()
+    {
+        node = null;
+    }
+
+    // public TreeNode insertNewNode(TreeNode node, int value)
+    // {
+    //     TreeNode tn = new TreeNode();
+    //     tn.data = value;
+    //     tn.left = null;
+    //     tn.right = null;
+
+    //     return tn;
+    // }
+    
+    // public TreeNode insertNode(TreeNode node , int value)
+    // {
+    //     if(node == null)
+    //     {
+    //         return insertNewNode(node, value);
+    //     }
+    //     else if(value < node.data)
+    //     {
+    //         node.left = insertNode(node.left, value);
+    //     }
+    //     else if(value > node.data)
+    //     {
+    //         node.right = insertNewNode(node.right, value);
+    //     }
+
+    //     return node;
+    // }
+
+    // Tree Traversal
+    //  In-Order traversal
+
+    public void preOrderTraversal(TreeNode node)
+    {
+        if(node == null)
+        {
+            return;
+        }
+        else
+        {
+
+            System.out.print(node.data + "\t");
+            preOrderTraversal(node.left);
+            preOrderTraversal(node.right);
+        }
+    }
+}
+
+class BinaryTreeTraversalDemo
+{
+    public static void main(String[] args) {
+        BinaryTreeTraversal btt = new BinaryTreeTraversal();
+        btt.node = new TreeNode(1);
+        btt.node.left = new TreeNode(2);
+        btt.node.right = new TreeNode(3);
+        btt.node.left.left = new TreeNode(4);
+        btt.node.left.right = new TreeNode(5);
+
+        btt.preOrderTraversal(btt.node);
+        
+    }
+}
