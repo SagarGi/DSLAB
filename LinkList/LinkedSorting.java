5ad221cccb53b77c7011106f026ae7c00818f639
SagarGi <sagargurung1001@gmail.com>
2020-05-20 19:59:34 +0545

    Sorting

---
+++ LinkList/LinkedSorting.java
@@ -0,0 +1,99 @@
+class LinkedList
+{
+    int data;
+    LinkedList next;
+}
+public class LinkedSorting {
+    public LinkedList add(LinkedList head, int value)
+    {
+        LinkedList l = new LinkedList();
+        l.data = value;
+        l.next = null;
+      if(head == null)
+      {
+          head = l;
+      }
+      else
+      {
+        LinkedList node = head;
+        while(node.next != null)
+        {
+            node = node.next;
+        }
+        node.next = l;
+      }
+
+      return head;
+    }
+
+    public void headDisplay(LinkedList head)
+    {
+        LinkedList node = head;
+        while(node.next != null)
+        {
+            System.out.println(node.data);
+            node = node.next;
+        }
+        System.out.println(node.data);
+    }
+
+    public void sortList(LinkedList head)
+    {
+        if(head != null && head.next != null)
+        {
+            LinkedList index = head;
+            while(index.next != null)
+            {
+                LinkedList node = index.next;
+                while(node.next != null)
+                {
+                    if(index.data > node.data)
+                    {
+                        // we swap the two
+                        int temp = index.data;
+                        index.data = node.data;
+                        node.data = temp;
+                    }
+                    node = node.next;
+                }
+                // for last element comparision
+                if(index.data > node.data)
+                {
+                    // we swap the two
+                    int temp = index.data;
+                    index.data = node.data;
+                    node.data = temp;
+                }
+
+                index = index.next;
+            }
+        }
+
+        else
+        {
+            System.out.println("The List is null!!");
+        }
+       
+    }
+}
+
+class LinkedSortingDemo
+{
+    public static void main(String[] args) {
+        LinkedSorting ls = new LinkedSorting();
+        LinkedList head = null;
+        head = ls.add(head, 9);
+        head = ls.add(head, 7);
+        head = ls.add(head, 2);
+        head = ls.add(head, 5);
+        head = ls.add(head, 45);
+        head = ls.add(head, 44);
+        head = ls.add(head, 23);
+        head = ls.add(head, 20);
+        head = ls.add(head, 100);
+        ls.sortList(head);
+        ls.headDisplay(head);
+        
+
+}
+}
