a7db69159725e5f52eac6413586376be215380fc
SagarGi <sagargurung1001@gmail.com>
2020-06-04 11:54:23 +0545

    2way

---
+++ MyLeetCode/TwoCityScheculing.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class TwoCityScheculing {
+    public int twoCitySchedCost(int[][] costs) {
+        int cost = 0;
+
+        // use comparator
+
+        Arrays.sort(costs, (a , b) -> 
+        {
+            return (a[0]- a[1]) - (b[0] - b[1]);
+        });
+
+        for(int i = 0; i < costs.length / 2; i++)
+        {
+            cost = cost + costs[i][0];
+        }
+        for(int i = costs.length / 2;  i < costs.length; i++)
+        {
+            cost = cost + costs[i][1];
+        }
+        
+        return cost;
+    }
+}
+
+class TwoCityScheculingDemo
+{
+    public static void main(String[] args) {
+        TwoCityScheculing tcs = new TwoCityScheculing();
+        int [][] costs = {{10,20},{30,200}, {400,50}, {30,20}};
+        System.out.println(tcs.twoCitySchedCost(costs));
+    }
+}
