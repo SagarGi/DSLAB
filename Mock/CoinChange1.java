91a497cfb3e68980dd75aa13acfedd490900dbd5
SagarGi <sagargurung1001@gmail.com>
2020-06-08 15:52:09 +0545

    coinchangedynamic

--- Mock/CoinChange1.java
+++ Mock/CoinChange1.java
@@ -26,8 +26,45 @@ public class CoinChange1 {
             return count;
         }
 
-    return -1;
+     return -1;
         
+    }
+
+
+    public int coinChangeDynamicProgramming(int[] coins, int amount) {
+       
+        int dp[][] = new int[coins.length + 1][amount + 1];
+        int row = dp.length;
+        int col = dp[0].length;
+
+        for(int i = 0; i < row; i++)
+        {
+            dp[i][0] = 0;
+        }
+
+        for(int i = 0; i < col; i++)
+        {
+            dp[0][i] = i;
+        }
+
+
+        for(int i = 1; i < row; i++)
+        {
+            for(int j =  1; j < col; j ++)
+            {
+                if(coins[i - 1] > j)
+                {
+                    dp[i][j] = dp[i - 1][j];
+                }
+                else
+                {
+                    dp[i][j] = Math.min(dp[i - 1][j], 1 + dp[i][j - coins[i - 1]]);
+                }
+            }
+        }
+
+            
+        return dp[row - 1][col - 1]; // this gives only correct answer when the amount sum exists. 
     }
     
 }
@@ -36,9 +73,9 @@ class CoinChange1Demo
 {
     public static void main(String[] args) {
         CoinChange1 c1 = new CoinChange1();
-        int coins[] = {1,2,5};
-        int amount = 11;
-        System.out.println(c1.coinChangeGreedyAlgorithm(coins, amount));
+        int coins[] = {2};
+        int amount = 10;
+        System.out.println(c1.coinChangeDynamicProgramming(coins, amount));
 
     }
 }
