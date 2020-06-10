01672391575531f54b1bfcf4ec99678f576a1bb6
SagarGi <sagargurung1001@gmail.com>
2020-06-10 20:56:07 +0545

    usinghashmap

--- Mock/TwoSum.java
+++ Mock/TwoSum.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
 
@@ -18,6 +19,21 @@ public class TwoSum {
 
         return result;
     }
+    public int[] twoSumHashMap(int nums[], int target)
+    {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(map.containsKey(target - nums[i]))
+            {
+                return new int[]{(map.get(target -nums[i])) , i};
+            }
+            map.put(nums[i], i);
+        }
+
+        return new int[2];
+    }
 }
 
 class TwoSumDemo
@@ -26,7 +42,7 @@ class TwoSumDemo
         int nums[] = {3,4,2};
         int target = 6;
         TwoSum ts = new TwoSum();
-        int result[] = ts.twoSum(nums, target);
+        int result[] = ts.twoSumHashMap(nums, target);
         System.out.println(result[0]);
         System.out.println(result[1]);
     }
