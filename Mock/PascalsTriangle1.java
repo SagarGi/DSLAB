7a853e883be47a31674e2a03d2efd5bf9586b7c3
SagarGi <sagargurung1001@gmail.com>
2020-06-14 12:56:42 +0545

    pascal2

--- Mock/PascalsTriangle1.java
+++ Mock/PascalsTriangle1.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class PascalsTriangle1
 {
-    public List<List<Integer>> generate(int numRows) {
+    public List<List<Integer>> generate1(int numRows) {
         // solving by dynamic programming
 
         int dp[][] = new int[numRows][numRows];
@@ -31,6 +31,37 @@ public class PascalsTriangle1
 
         return result;
     }
+
+    public List<Integer> generate2(int rowIndex) {
+        // solving by dynamic programming
+
+        int dp[][] = new int[rowIndex + 1][rowIndex + 1];
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i <= rowIndex; i++)
+        {
+           
+            for(int j = 0; j <= i; j++)
+            {
+                if(j == 0 || j == rowIndex)
+                {
+                    dp[i][j] = 1;
+                }
+                else
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                }
+            }
+        }
+
+        for(int i = rowIndex; i <= rowIndex; i++)
+        {
+            for(int j = 0; j < dp[rowIndex].length; j++)
+            {
+                list.add(dp[i][j]);
+            }
+        }
+        return list;
+    }
 }
 
 class PascalsTriangle1Demo
@@ -38,7 +69,9 @@ class PascalsTriangle1Demo
     public static void main(String[] args) {
         int numRows = 0;
         PascalsTriangle1 pt = new PascalsTriangle1();
-        List<List<Integer>> res = pt.generate(numRows);
-        System.out.println(res);
+        List<List<Integer>> res = pt.generate1(numRows);
+        // System.out.println(res);
+        List<Integer> result = pt.generate2(4);
+        System.out.println(result);
     }
 }
