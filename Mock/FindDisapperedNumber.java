ef97a75e68b95debb7ffce8eaeb99bc118403eff
SagarGi <sagargurung1001@gmail.com>
2020-06-14 16:29:43 +0545

    finddisappeared

---
+++ Mock/FindDisapperedNumber.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class FindDisapperedNumber {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        
+        int countArray[] = new int[nums.length + 1];
+
+        for(int i = 0; i < nums.length; i++)
+        {
+            countArray[nums[i]]++;
+        }
+
+        for(int i = 1; i < nums.length; i++)
+        {
+            if(countArray[i] == 0)
+            {
+                list.add(i);
+            }
+        }
+        
+        
+        return list;
+    }
+}
+
+class FindDisapperedNumberDemo
+{
+    public static void main(String[] args) {
+        int nums[] = {4,3,2,7,8,2,3,1};
+        FindDisapperedNumber fdn = new FindDisapperedNumber();
+        System.out.println(fdn.findDisappearedNumbers(nums));
+    }
+}
