d4c22517638df4d85bc62d41019e756f67b19ef1
SagarGi <sagargurung1001@gmail.com>
2020-10-04 11:12:53 +0545

    use of hashset

--- Mock/SingleNumber.java
+++ Mock/SingleNumber.java
@@ -1,36 +1,29 @@
-import java.util.HashMap;
+
+import java.util.HashSet;
 
 public class SingleNumber {
     public int singleNumber(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++)
-        {
-            if(!map.containsKey(nums[i]))
-            {
-                map.put(nums[i], 1);
-            }
-            else
-            {
-                map.put(nums[i], map.get(nums[i]) + 1);
+        HashSet<Integer> set = new HashSet<Integer>();
+
+        for (int i : nums) {
+            if (set.contains(i)) {
+                set.remove(i);
+            } else {
+                set.add(i);
             }
         }
 
-        for(Integer i : map.keySet())
-        {
-            if(map.get(i) == 1)
-            {
-                return i;
-            }
+        for (int i : set) {
+            return i;
         }
-       
-        return 0;
+
+        return -1;
     }
 }
 
-class SingleNumberDemo
-{
+class SingleNumberDemo {
     public static void main(String[] args) {
-        int nums [] = {4,1,2,1,2};
+        int nums[] = { 4, 1, 2, 1, 2 };
         SingleNumber sn = new SingleNumber();
         System.out.println(sn.singleNumber(nums));
     }
