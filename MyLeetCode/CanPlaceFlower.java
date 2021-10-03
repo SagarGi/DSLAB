a5b7693c7828dd9c96f0594edaac208820117684
SagarGi <sagargurung1001@gmail.com>
2021-10-03 14:45:18 +0545

    canplaceflower optimized

--- MyLeetCode/CanPlaceFlower.java
+++ MyLeetCode/CanPlaceFlower.java
@@ -1,68 +1,23 @@
 public class CanPlaceFlower {
 
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-
-        boolean result = false;
-        int count = 0;
-        if(flowerbed.length == 1 && flowerbed[0] == 0 && n <= 1)
-        {
-            result = true;
-        }
-        else if(flowerbed.length == 2)
-        {
-            if(flowerbed[0] == 0 && flowerbed[1] == 0 && n <= 1)
-            {
-                result = true;
+        int i = 0, count = 0;
+        while (i < flowerbed.length) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0)
+                    && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                count++;
             }
+            i++;
         }
-        else
-        {
-            for(int i = 0; i < flowerbed.length; i++)
-            {
-                if(i == 0)
-                {
-                    if(flowerbed[i] == 0 && flowerbed[i + 1] == 0)
-                    {
-                        count++;
-                        flowerbed[i] = 1;
-                    }
-                }
-                else if(i == flowerbed.length -1)
-                {
-                    if(flowerbed[i-1] == 0 && flowerbed[i] == 0)
-                    {
-                        count++;
-                        flowerbed[i] = 1;
-                    }
-                }
-                else
-                {
-                    if(flowerbed[i-1] == 0 && flowerbed[i] == 0 && flowerbed[ i+ 1] == 0)
-                    {
-                        count++;
-                        flowerbed[i] = 1;
-                    }
-                }
-            }
-        }
-
-        System.out.println(count);
-
-        if(n <= count)
-        {
-            result = true;
-        }
-        return result;
-        
+        return count >= n;
     }
-    
+
 }
 
-class CanPlaceFlowerDemo
-{
-    public static void main(String args[])
-    {
-        int flowerbed[] = {0,0,1,0,0,0,1};
+class CanPlaceFlowerDemo {
+    public static void main(String args[]) {
+        int flowerbed[] = { 0, 0, 1, 0, 0, 0, 1 };
         int n = 2;
         CanPlaceFlower cpf = new CanPlaceFlower();
         System.out.println(cpf.canPlaceFlowers(flowerbed, n));
@@ -70,3 +25,5 @@ class CanPlaceFlowerDemo
 }
 
 // code works but is not optimised
+
+// i have optimized it
